0c589bb5ed14ba813ad8c9b3781c5a39f31a72cf
RajGM <rajgmsocial19@gmail.com>
2022-06-15 21:41:35 +0530

    Build: Toast Components integrated successfully

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 
 import Link from 'next/link'
 import Loader from '../components/Loader'
+import toast from 'react-hot-toast';
 
 export default function Home() {
   return (
@@ -12,6 +13,11 @@ export default function Home() {
       <Link href="/admin">Admin</Link>
       <br></br>
       <Link prefetch={false} href={{pathname:'/[username]',query:{username:'RajGM'}}}><a>RajGM Profile</a></Link>
+      <div>
+        <button onClick={()=> toast.success('hello toast!')}>
+          Toast Me
+        </button>
+      </div>
     </div>
   )
 }
