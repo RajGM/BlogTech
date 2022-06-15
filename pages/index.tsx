9628bac699d7046de0b359ee5b303d19c24f7190
RajGM <rajgmsocial19@gmail.com>
2022-06-15 21:16:48 +0530

    Build: Loader Component Completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import Link from 'next/link'
+import Loader from '../components/Loader'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Loader show></Loader>
       <Link href="/admin">Admin</Link>
+      <br></br>
+      <Link prefetch={false} href={{pathname:'/[username]',query:{username:'RajGM'}}}><a>RajGM Profile</a></Link>
     </div>
   )
 }
